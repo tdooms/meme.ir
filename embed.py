8fc0428795479b5a70563ef43c8bec4b52f841fa
thomas <thomas@dooms.eu>
2022-11-18 14:47:15 +0100

    weeeeeeee

--- embed.py
+++ embed.py
@@ -1,22 +1,23 @@
 import time
-
 import numpy as np
 from matplotlib import pyplot as plt
-from sentence_transformers import SentenceTransformer
-import polars as pl
-from sklearn.manifold import TSNE
+import pandas as pd
+from sklearn.decomposition import PCA
 import seaborn as sns
+from sklearn.manifold import TSNE
 
 
 def embed(model, boxes): return model.encode('. '.join(boxes)).tolist()
 
 
 def store():
+    from sentence_transformers import SentenceTransformer
+
     model = SentenceTransformer('all-MiniLM-L6-v2')
     start = time.time()
 
-    memes = pl.read_ipc("data/memes.feather")
-    memes = memes.with_columns([pl.col('boxes').apply(lambda x: embed(model, x)).alias('features')])
+    memes = pd.read_feather("data/memes.feather")
+    memes["features"] = memes.apply(lambda x: embed(model, x))
 
     # computed features in 5217.94 seconds - on my pc
     print(f"computed features in {time.time() - start:.2f} seconds")
@@ -24,26 +25,30 @@ def store():
     memes.write_ipc("data/features.feather")
 
 
+def sample():
+    memes = pd.read_feather("data/features.feather")
+    memes.sample(1000).reset_index(drop=True).to_feather("data/sample.feather")
+
+
 def visualise():
-    memes = pl.read_ipc("data/features.feather")
-
-    # memes = memes.with_columns(
-    #     [pl.col('features').map(lambda x: TSNE(n_components=2).fit_transform(x)).alias('tsne')]
-    # )
-    # print(memes)
-
     start = time.time()
-
-    features = np.array(memes['features'].to_list())
-    projected = TSNE(n_components=2).fit_transform(features)
-
+    memes = pd.read_feather("data/sample.feather")
     print(f"read features in {time.time() - start:.2f} seconds")
 
+    start = time.time()
+    features = np.array(memes['features'].to_list())
+    projected = TSNE(n_components=2).fit_transform(features)
+    print(f"computed PCA in {time.time() - start:.2f} seconds")
+
     # visualise the t-sne per meme name in a scatter plot with seaborn
-    sns.scatterplot(x=projected[:, 0], y=projected[:, 1], hue=memes['name'])
-    plt.show()
+    # start = time.time()
+    # sns.scatterplot(x=projected[:, 0], y=projected[:, 1], hue=memes['name'])
+    # plt.legend([], [], frameon=False)
+    # plt.show()
+    # print(f"computed plot in {time.time() - start:.2f} seconds")
 
 
 if __name__ == '__main__':
     # store()
+    # sample()
     visualise()
